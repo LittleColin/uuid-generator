a8e48ad4b92386841bc861fa2a36ccfc1a227def
littlecolin <cybercohesion.git@outlook.com>
2018-07-31 14:52:04 +0100

    Fix

--- brigade.js
+++ brigade.js
@@ -1,8 +1,9 @@
 const { events, Job } = require("brigadier");
-const { devops } = require("./devops");
+const { TestClass } = require("./devops");
 
 events.on("push", () => {
     consle.log('Testing');
-    console.log(devops.hello("world"));
+    var t = new TestClass();
+    console.log(t.hello("world"));
 });
 
